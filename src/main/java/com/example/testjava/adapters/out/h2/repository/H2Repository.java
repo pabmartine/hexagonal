36e7cf7e3542997e931c6969da6df5eb8290fdc7
Pablo Martín Escribano <pablo.martin-escribano@capgemini.es>
2021-10-01 07:40:02 +0200

    test

--- src/main/java/com/example/testjava/adapters/out/h2/repository/H2Repository.java
+++ src/main/java/com/example/testjava/adapters/out/h2/repository/H2Repository.java
@@ -25,7 +25,7 @@ public interface H2Repository extends JpaRepository<H2PriceEntity, Integer> {
 	 * @param productId product id
 	 * @param brandId   brand id
 	 * @param pageable  page object from spring
-	 * @return list of prices
+	 * @return          list of prices
 	 */
 	@Query("SELECT p FROM H2PriceEntity p where (:date between p.startDate and p.endDate) and p.productId = :productId and p.brandId = :brandId")
 	public List<H2PriceEntity> findAll(@Param("date") Date date, @Param("productId") Integer productId,
